1a99c3ef0b07f98af2df1f62b0d957b51e2a1466
RizWaaN3024 <rizwaanans786@gmail.com>
2025-05-09 03:04:06 +0530

    [Navbar] Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
+import { NAV_LINKS, NavLink } from '@/data/data'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
     return (
         <nav className="lg:container mx-auto px-4 py-16">
-            <div className="">
+            <div className="flex items-center justify-between">
                 <div>
-                    <h1 className="font-medium text-[32px]"><span className="text-[#12F7D6]">{`<R/>`}</span>RizwaanAnsari</h1>
+                    <h1 className="font-medium text-[32px]"><span className="text-[#12F7D6]">{`<R/>`}</span> RizwaanAnsari</h1>
+                </div>
+                <div>
+                    <ul className="flex list-none">
+                        {NAV_LINKS.map((navItem: NavLink, id: number) => {
+                            return (
+                                <li key={`navLink-${id}`}>
+                                    <Link href={navItem.link} className="font-medium text-2xl px-8">{navItem.label}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </div>
             </div>
         </nav>
---
+++ src/data/data.ts
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export interface NavLink {
+    id: number;
+    label: string;
+    link: string;
+}
+
+interface SocialLink {
+    id: number;
+    label: string;
+    icon: React.ElementType;
+    link: string;
+}
+
+export const NAV_LINKS: NavLink[] = [
+    {
+        id: 1,
+        label: "Home",
+        link: "/home"
+    }, 
+    {
+        id: 2,
+        label: "Blogs",
+        link: "/blog"
+    }
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+    {
+        id: 1,
+        label: "Instagram",
+        icon: FaInstagram,
+        link: "",
+    },
+    {
+        id: 2,
+        label: "Github",
+        icon: FaGithub,
+        link: "",
+    },
+    {
+        id: 3,
+        label: "LinkedIn",
+        icon: FaLinkedin,
+        link: "",
+    },
+]
