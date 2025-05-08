b6da585bb22133b7237148f18850cf6f2a5d558a
RizWaaN3024 <rizwaanans786@gmail.com>
2025-05-09 02:32:19 +0530

    [Navbar] Navbar initial setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="lg:container mx-auto px-4 py-16">
+            <div className="">
+                <div>
+                    <h1 className="font-medium text-[32px]"><span className="text-[#12F7D6]">{`<R/>`}</span>RizwaanAnsari</h1>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
