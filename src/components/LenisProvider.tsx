0581d7f8a446aebac0c0d4b4aa6b2e98915fd494
RizWaaN3024 <rizwaanans786@gmail.com>
2025-07-26 23:04:12 +0530

    Code fixes

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -3,7 +3,7 @@
 import { useLenis } from "@/app/hooks/useLenis";
 
 
-export default function LenisProvider({ children }: Readonly<{ children: React.ReactNode}>) {
+export default function LenisProvider({ children }: Readonly<{ children: React.ReactNode }>) {
     useLenis();
-    return <>{ children }</>
+    return <>{children}</>
 }
